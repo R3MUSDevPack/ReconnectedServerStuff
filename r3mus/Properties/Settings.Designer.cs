4d1b8541d5fdc9e7b75cbf945e01539496b4a2fd
ClydeenMarland <clydeenmarland@gmail.com>
2016-05-19 08:39:59 +0100

    Hateword censoring implemented in Jarvis

--- r3mus/Properties/Settings.Designer.cs
+++ r3mus/Properties/Settings.Designer.cs
@@ -347,5 +347,22 @@ namespace r3mus.Properties {
                 return ((string)(this["SlackWebhook"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfString xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <string>gay</string>
+  <string>fag</string>
+  <string>rape</string>
+  <string>fuck</string>
+  <string>wanker</string>
+  <string>twat</string>
+</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection CensoredWords {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["CensoredWords"]));
+            }
+        }
     }
 }
